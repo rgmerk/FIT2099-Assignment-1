3c64e5f471ab40540e481d096f5f5b96d6ef3925
Asel27 <aikor1@student.monash.edu>
2017-01-25 00:40:27 +1100

    Fixed error in assertion and java doc
    assertion for takeDamage() was changed to make sure the damage was
    greater than or equal to 0. Added assertion to the symbol to ensure "."
    are avoided and strings have a length of 1. Corrections to the Java doc

--- FIT2024_Assignment/src/hobbit/HobbitEntity.java
+++ FIT2024_Assignment/src/hobbit/HobbitEntity.java
@@ -39,17 +39,19 @@ public class HobbitEntity extends Entity implements HobbitEntityInterface {
 	
 	/**
 	 * Setter for the symbol of the Entity
-	 *<p>Single character strings are preferred over multi character strings to avoid confusion in the text interface
-	 * Try avoiding "." as an empty space is represented on the text interface using the ".", hence your Entity might not be visible 
-	 * 	and be mistaken for an empty space
-	 * These constraints are however for the current Text interface and would not have to be followed for a GUI for example</p>
+	 *<p>Single character strings are preferred over multi character strings to avoid confusion in the text interface</p>
+	 *
+	 *<p>Try avoiding "." as an empty space is represented on the text interface using the ".", hence your Actor might not be visible 
+	 * 	and be mistaken for an empty space </p>
+	 * <p>These constraints are however for the current Text interface and would not have to be followed for a GUI for example</p>
 	 * 
 	 * TODO : assertions for a single character and .?
 	 * 
-	 * @param the symbol by which the actor is represented by. 
+	 * @param the symbol by which the entity is represented by. 
 	 *
 	 */
 	public void setSymbol(String s) {
+		assert (!s.matches(".") && (s.length()==1)):"Symbol should be a single charater and not a '.'";
 		symbol = s;
 	}
 
@@ -82,7 +84,7 @@ public class HobbitEntity extends Entity implements HobbitEntityInterface {
 	public void takeDamage(int damage) {
 		//assertion to ensure the damage is not negative
 		//this assertion might have to be removed if the entities can be healed, sounds like a hack
-		assert (damage < 0)	:"damage on entity must not be negative";
+		assert (damage >= 0)	:"damage on entity must not be negative";
 		
 		this.hitpoints -= damage;
 	}
