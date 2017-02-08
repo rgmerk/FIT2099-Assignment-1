1e0e9f2e2082ff1f000e67698dbe8f89adb09e8e
Asel27 <aikor1@student.monash.edu>
2017-02-08 22:19:01 +1100

    Improvements to Javadoc

--- FIT2024_Assignment/src/hobbit/HobbitEntity.java
+++ FIT2024_Assignment/src/hobbit/HobbitEntity.java
@@ -16,8 +16,14 @@ import edu.monash.fit2024.simulator.matter.Entity;
 import edu.monash.fit2024.simulator.userInterface.MessageRenderer;
 
 public class HobbitEntity extends Entity implements HobbitEntityInterface {
+	
+	/**A string symbol that represents this <code>HobbitEntity</code>, suitable for display*/
 	private String symbol;
-	protected HashSet<Capability> capabilities;//a HashSet of what the entity can do (capabilities)
+	
+	/**A set of <code>Capability</code>s of this <code>HobbitEntity</code>*/
+	protected HashSet<Capability> capabilities;
+	
+	/**The amount of <code>hitpoints</code> of this Hobbit Entity.*/
 	protected int hitpoints = 0; // Not all non-actor entities will make use of this
 
 	protected HobbitEntity(MessageRenderer m) {
@@ -29,8 +35,8 @@ public class HobbitEntity extends Entity implements HobbitEntityInterface {
 	 * @see hobbit.HobbitEntityInterface#getSymbol()
 	 */
 	/**
-	 * Getter for the symbol
-	 * @return symbol : by which the entity is represented by
+	 * Getter for the <code>symbol</code> of the <code>HobbitEntity</code>
+	 * @return symbol @see {@link #symbol}
 	 */
 	@Override
 	public String getSymbol() {
@@ -38,17 +44,9 @@ public class HobbitEntity extends Entity implements HobbitEntityInterface {
 	}
 	
 	/**
-	 * Setter for the symbol of the Entity
-	 *<p>Single character strings are preferred over multi character strings to avoid confusion in the text interface</p>
+	 * Setter for the <code>symbol</code> of the <code>HobbitEntity</code>
 	 *
-	 *<p>Try avoiding "." as an empty space is represented on the text interface using the ".", hence your Actor might not be visible 
-	 * 	and be mistaken for an empty space </p>
-	 * <p>These constraints are however for the current Text interface and would not have to be followed for a GUI for example</p>
-	 * 
-	 * TODO : assertions for a single character and .?
-	 * 
-	 * @param the symbol by which the entity is represented by. 
-	 *
+	 * @param s @see {@link #symbol} 
 	 */
 	public void setSymbol(String s) {
 		assert (!s.matches(".") && (s.length()==1)):"Symbol should be a single charater and not a '.'";
@@ -59,33 +57,40 @@ public class HobbitEntity extends Entity implements HobbitEntityInterface {
 	 * <p>Returns true if this Entity has the given capability, false otherwise.</p>
 	 * <p>Wrapper for HashSet<Capability>.contains().</p>
 	 * 
-	 * @param the Capability to search for
+	 * @param c the Capability to search for
 	 */
 	@Override
 	public boolean hasCapability(Capability c) {
 		return capabilities.contains(c);
 	}
 
+	/**
+	 * Getter for the <code>hitpoints</code> of <code>HobbitEntity</code>
+	 * 
+	 * @return @see {@link #hitpoints}
+	 */
 	public int getHitpoints() {
 		return hitpoints;
 	}
 	
+	/**
+	 * Setter for the <code>hitpoints</code> of <code>HobbitEntity</code>
+	 * 
+	 * @param p @see {@link #hitpoints}
+	 */
 	public void setHitpoints(int p) {
 		hitpoints = p;
 	}
 	
 	/**
-	 * Method insist damage by reducing a certain amount of damage from the enitiy's hit points
+	 * Method insists damage on this <code>HobbitEntity</code> by reducing a certain amount of <code>damage</code> from this <code>HobbitEntity</code>'s <code>hitpoints</code>
 	 * 
-	 * @param the amount of damage to be done on the actor.
-	 * 
-	 *TODO: Add an assertion to make sure the damage given is a positive value or zero since a negative value could increase the hitpoints
+	 * @param damage the amount of <code>hitpoints</code> to be reduced
 	 */
 	public void takeDamage(int damage) {
-		//assertion to ensure the damage is not negative
-		//this assertion might have to be removed if the entities can be healed, sounds like a hack
-		assert (damage >= 0)	:"damage on entity must not be negative";
-		
+		//assertion to ensure the damage is not negative. Negative damage could increase the HobbitEntity's hitpoints
+		assert (damage >= 0)	:"damage on HobbitActor must not be negative";
 		this.hitpoints -= damage;
 	}
+	
 }
