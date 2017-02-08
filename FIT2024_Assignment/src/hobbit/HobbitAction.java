ed6fabcb8685d07e79747cac043362d2d8b21194
Asel27 <aikor1@student.monash.edu>
2017-02-08 22:15:27 +1100

    Minor improvements to the Javadoc

--- FIT2024_Assignment/src/hobbit/HobbitAction.java
+++ FIT2024_Assignment/src/hobbit/HobbitAction.java
@@ -6,6 +6,7 @@
 /*
  * Change log
  * 2017-01-20: Extensions to the Javadoc
+ * 2017-02-08: The execute will only call the act of the HobbitActor is alive
  * 
  */
 package hobbit;
@@ -17,6 +18,7 @@ import edu.monash.fit2024.simulator.userInterface.MessageRenderer;
 
 public abstract class HobbitAction extends Action implements HobbitActionInterface {
 	
+	/**The entity manager of the world which keeps track of <code>HobbitEntity</code>s and their <code>HobbitLocation</code>s*/
 	private static final EntityManager<HobbitEntityInterface, HobbitLocation> entityManager = MiddleEarth.getEntitymanager();
 
 	public HobbitAction(MessageRenderer m) {
@@ -34,26 +36,28 @@ public abstract class HobbitAction extends Action implements HobbitActionInterfa
 	}
 	
 	/**
-	 * Execute the action by the actor. If the Actor is not a HobbitActor, is dead or is null this method does nothing
+	 * Execute the action by the actor. If the Actor is not a HobbitActor, is null or is a dead HobbitActor this method does nothing
 	 * 
-	 * TODO A cleaner way to make sure dead actors don't act would be to remove the event of the dead actor from the scheduled events
 	 */
 	public void execute(Actor<?> a) {
-		if (a instanceof HobbitActor && !((HobbitActor) a).isDead()) 
+		if (a instanceof HobbitActor && !((HobbitActor)a).isDead()) 
 			act((HobbitActor) a);
 
 	}
 
 	public abstract void act(HobbitActor a);
 	
+	/**
+	 * compareTo method to order HobbitActionInterface objects in alphabetical order
+	 */
 	public int compareTo(HobbitActionInterface other) {
 		return this.getDescription().compareTo(other.getDescription());
 	}
 
 	/**
-	 * Getter for the entity manager which keeps track of Entities and their Locations
+	 * Getter for the <code>entityManager</code>
 	 * 
-	 * @return entityManager of the world
+	 * @return entityManager of the world @see {@link #entityManager}
 	 */
 	public static EntityManager<HobbitEntityInterface, HobbitLocation> getEntitymanager() {
 		return entityManager;
