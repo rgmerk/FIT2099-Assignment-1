eced7e625c54b45c86e8d5fbc25427155fa57a10
Asel27 <aikor1@student.monash.edu>
2017-02-09 15:28:19 +1100

    Bug Fix
    Fixed the bug where it was possible to chop and attack using a Sword
    that had negative hitpoints. The takeDamage method will change the
    descriptions of the broken Sword and will remove the CHOPPER and WEAPON
    capabilities from the Sword so that a broken Sword cannot be used to
    chop or to attack.

--- FIT2024_Assignment/src/hobbit/entities/Sword.java
+++ FIT2024_Assignment/src/hobbit/entities/Sword.java
@@ -6,6 +6,9 @@
  */
 /*
  * 2017/02/04 Removed the Unicode symbol of the sword to a 's' (asel)
+ * 2017/02/08 takeDamage method was overridden to change it's descriptions when the Swords hitpoints are zero or less
+ * 			  the takeDamage method will also remove the CHOPPER and WEAPON capabilities from the Sword as it should not be possible
+ * 			  to attack or chop with a broken Sword (asel)
  */
 package hobbit.entities;
 
@@ -33,10 +36,40 @@ public class Sword extends HobbitEntity {
 	/**
 	 * A symbol to represent the Sword on the user interface
 	 * 
-	 * @return a single character string " " representing a sword
+	 * @return a single character string "s" representing a sword
 	 */
 	public String getSymbol() {
 		return "s"; 
 	}
+
+	/**
+	 * Method insists damage on this <code>Sword</code> by reducing a certain 
+	 * amount of <code>damage</code> from this <code>Sword</code>s <code>hitpoints</code>
+	 * <p>
+	 * This method will also change this <code>Sword</code>s <code>longDescription</code> to
+	 * "A broken sword that was once gleaming"  and this <code>Sword</code>s <code>shortDescription</code> to
+	 * "a broken sword"if the <code>hitpoints</code> after taking the damage is zero or less.
+	 * <p>
+	 * If the <code>hitpoints</code> after taking the damage is zero or less, this method will remove the 
+	 * <code>CHOPPER</code> and <code>WEAPON</code> capabilities from this <code>Sword</code> since a broken sword 
+	 * cannot be used to <code>Chop</code> or <code>Attack</code>.
+	 * 
+	 * @author 	Asel
+	 * @param 	damage the amount of <code>hitpoints</code> to be reduced
+	 */
+	@Override
+	public void takeDamage(int damage) {
+		super.takeDamage(damage);
+		
+		if (this.hitpoints<=0) {
+			this.shortDescription = "a broken sword";
+			this.longDescription  = "A broken sword that was once gleaming";
+			
+			this.capabilities.remove(Capability.CHOPPER);
+			this.capabilities.remove(Capability.WEAPON);
+		}
+	}
+	
+	
 
 }
