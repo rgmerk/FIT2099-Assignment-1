3a96816d76f66fa999ab80fe8c35ffd443dff659
Asel27 <aikor1@student.monash.edu>
2017-02-09 15:28:52 +1100

    Fixed minor issues with Javadoc

--- FIT2024_Assignment/src/hobbit/HobbitGrid.java
+++ FIT2024_Assignment/src/hobbit/HobbitGrid.java
@@ -7,8 +7,8 @@
  * Change log
  * 2017-01-20: 	Bug fix where the location of width 8 used to display a location of width 7
  * 2017-02-02: 	Removed the render method and the location width attributes. The rendering of the map
- * 				and displaying it is now the job of the TextInterface class. The dependency with EntityManager package was hence removed
- * 				and his resulted in a simpler HobbitGrid class (asel) 
+ * 				and displaying it is now the job of the UI. The dependency with EntityManager package was hence removed
+ * 				and this resulted in a simpler HobbitGrid class (asel) 
  */
 package hobbit;
 
