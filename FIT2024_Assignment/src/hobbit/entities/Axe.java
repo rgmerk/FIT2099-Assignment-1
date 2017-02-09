609ace5f2ed23787e9138bcaa970ed3cf4bcd236
Asel27 <aikor1@student.monash.edu>
2017-02-09 15:27:26 +1100

    Bug Fix
    Fixed the bug where it was possible to chop using an Axe that had
    negative hitpoints. The takeDamage method will change the descriptions
    of the broken Axe and will remove the CHOPPER capability from the Axe so
    that a broken Axe cannot be used to chop.

--- FIT2024_Assignment/src/hobbit/entities/Axe.java
+++ FIT2024_Assignment/src/hobbit/entities/Axe.java
@@ -4,6 +4,12 @@
  * 
  *  @author ram
  */
+/*
+ * Change log
+ * 2017/02/08 takeDamage method was overridden to change it's descriptions when the Axes hitpoints are zero or less
+ * 			  the takeDamage method will also remove the CHOPPER capability from the Axe as it should not be possible
+ * 			  to chop with a broken Axe. (asel)
+ */
 package hobbit.entities;
 
 import hobbit.Capability;
@@ -31,6 +37,33 @@ public class Axe extends HobbitEntity {
 	 */
 	public String getSymbol() {
 		return "Æ";
+	}
+	
+	/**
+	 * Method insists damage on this <code>Axe</code> by reducing a certain 
+	 * amount of <code>damage</code> from this <code>Axe</code>s <code>hitpoints</code>
+	 * <p>
+	 * This method will also change this <code>Axe</code>s <code>longDescription</code> to
+	 * "A broken axe that was once shining" and this <code>Axe</code>s <code>shortDescription</code> to
+	 * "a broken axe"if the <code>hitpoints</code> after taking the damage is zero or less.
+	 * <p>
+	 * If the <code>hitpoints</code> after taking the damage is zero or less, this method will remove the 
+	 * <code>CHOPPER</code> capability from this <code>Axe</code> since a broken axe
+	 * cannot be used to <code>Chop</code>.
+	 * 
+	 * @author 	Asel
+	 * @param 	damage the amount of <code>hitpoints</code> to be reduced
+	 */
+	@Override
+	public void takeDamage(int damage) {
+		super.takeDamage(damage);
+		
+		if (this.hitpoints<=0) {
+			this.shortDescription = "a broken axe";
+			this.longDescription  = "A broken axe that was once shining";
+			
+			this.capabilities.remove(Capability.CHOPPER);
+		}
 	}
 
 }
