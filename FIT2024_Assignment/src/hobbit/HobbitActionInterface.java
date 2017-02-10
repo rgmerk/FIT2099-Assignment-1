2241cf1ee216a9f3eb14d260c46b0a2dd5051a25
Asel27 <aikor1@student.monash.edu>
2017-02-10 19:19:34 +1100

    Removed extension of Comparable
    The ActionInterface which the HobbitActionInterface extends already
    handles this

--- FIT2024_Assignment/src/hobbit/HobbitActionInterface.java
+++ FIT2024_Assignment/src/hobbit/HobbitActionInterface.java
@@ -8,7 +8,7 @@ package hobbit;
 
 import edu.monash.fit2024.simulator.matter.ActionInterface;
 
-public interface HobbitActionInterface extends ActionInterface, Comparable<HobbitActionInterface> {
+public interface HobbitActionInterface extends ActionInterface {
 	public boolean isMoveCommand();
 	public boolean canDo(HobbitActor a);
 	public void act(HobbitActor a);	
